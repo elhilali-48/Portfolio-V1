2cbc3c2219aaa67fc5874df8b23b74377a442d1d
Hilali <elhilali.abdelouahab@gmail.com>
2022-06-28 15:02:24 +0200

    First Commit 28/06/2022

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="overflow">
+    <v-app-bar
+      color="green"
+      dark
+      height ="90"
+      outlined
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Collapsing Bar</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+     <v-toolbar-title>EL HILALI Bar</v-toolbar-title>
+   
+    </v-app-bar>
+   
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
